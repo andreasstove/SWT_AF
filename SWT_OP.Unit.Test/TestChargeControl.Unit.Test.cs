939617e3c8e0704378f50df6e565d1c6a23af0d3
andreasstove <andreasstove@outlook.dk>
2020-10-24 10:04:20 +0200

    ChargeControl

---
+++ SWT_OP.Unit.Test/TestChargeControl.Unit.Test.cs
@@ -0,0 +1,31 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+
+namespace SWT_OP.Unit.Test
+{
+    [TestFixture]
+    public class TestChargeControl
+    {
+        private IUsbCharger _usbCharger;
+
+        private ChargeControl _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _usbCharger = Substitute.For<IUsbCharger>();
+
+            _uut = new ChargeControl(_usbCharger);
+        }
+
+        [Test]
+        public void testForCurrentLow()
+        {
+            _usbCharger.currentValueEvent += Raise.EventWith(new CurrentEventArgs { Current = 200 });
+            Assert.That(_uut.IsConnected, Is.EqualTo(true));
+        }
+
+       
+    }
+}
