8926ae34d30163b7d82a1747fbad7db18681dcb5
thomasgl710 <thomasgl10@gmail.com>
2020-10-26 21:49:50 +0100

    Main program

---
+++ SWT_OP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWT_OP
+{
+    class mainn
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej med dig");
+        }
+    }
+}
+
