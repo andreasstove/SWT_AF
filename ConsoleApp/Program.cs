7551743c604438e7f63bbc82015ea9624b2f20d9
thomasgl710 <thomasgl10@gmail.com>
2020-10-27 10:56:15 +0100

    threads kortere

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,34 +14,34 @@ namespace ConsoleApp
             UsbCharger usbCharger = new UsbCharger();
             ChargeControl charge = new ChargeControl(usbCharger);
             StationControl station = new StationControl(door, rFIDReader, display, charge);
-            Console.WriteLine("Det hele er nu initiaseret og kan køre som program.");
+            Console.WriteLine("Det hele er nu initialiseret og kan køre som program.");
             System.Threading.Thread.Sleep(5000);
             
             Console.WriteLine("Prøver at sætte id op til scanneren");
             System.Threading.Thread.Sleep(2000);
             rFIDReader.RfidDetect(100);
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             
             Console.WriteLine("Nu sættes telefonen til for at RFI'en bruges");
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             //prøv først at starte det uden´.
             usbCharger.ConnectPhone();
             rFIDReader.RfidDetect(110);
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             
             Console.WriteLine("Nu prøves med det forkerete id for at låse op og derefter prøves med det rigtige id");
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             rFIDReader.RfidDetect(120);
 
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             rFIDReader.RfidDetect(110);
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             Console.WriteLine("Nu åbnes døren for derefter at blive lukket");
 
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
 
             door.UnlockedDoor();
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(5000);
             door.LockedDoor();
             System.Threading.Thread.Sleep(10000);
 
