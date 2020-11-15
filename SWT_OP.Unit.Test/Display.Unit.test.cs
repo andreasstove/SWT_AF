9c1da0cd15e1762eb58620c7cf269366522502b5
thomasgl710 <thomasgl10@gmail.com>
2020-11-15 18:28:36 +0100

    ændret navne på test

--- SWT_OP.Unit.Test/Display.Unit.test.cs
+++ SWT_OP.Unit.Test/Display.Unit.test.cs
@@ -22,48 +22,47 @@ namespace SWT_OP.Unit.Test
         }
 
         [Test]
-        public void ShowConnectToPhone_test()
+        public void ShowConnectToPhone_Called_WriteLine()
         {
             _text = "Tilslut telefon";
             _uut.showConnectToPhone();
             _testDisplay.Received(1).WriteLine(_text);
         }
         [Test]
-        public void ShowReadRFID_test()
+        public void ShowReadRFID_Called_WriteLine()
         {
             _text = "Indlæs RFID";
             _uut.showReadRFID();
             _testDisplay.Received(1).WriteLine(_text);
         }
         [Test]
-        public void ShowConnectionToPhoneFailed_test()
+        public void ShowConnectionToPhoneFailed_Called_WriteLine()
         {
             _text = "Tilslutningsfejl";
             _uut.showConnectionToPhoneFailed();
             _testDisplay.Received(1).WriteLine(_text);
         }
         [Test]
-        public void ShowChargerCabinetIsOccupied_test()
+        public void ShowChargerCabinetIsOccupied_Called_WriteLine()
         {
             _text = "Ladeskab optaget";
             _uut.showChargerCabinetIsOccupied();
             _testDisplay.Received(1).WriteLine(_text);
         }
         [Test]
-        public void ShowRFIDMistake_test()
+        public void ShowRFIDMistake_Called_WriteLine()
         {
             _text = "RFID fejl";
             _uut.showRFIDMistake();
             _testDisplay.Received(1).WriteLine(_text);
         }
         [Test]
-        public void ShowRemovePhone_test()
+        public void ShowRemovePhone_Called_WriteLine()
         {
 
             _text = "Fjern telefon";
             _uut.showRemovePhone();
             _testDisplay.Received(1).WriteLine(_text);
         }
-
     }
 }
